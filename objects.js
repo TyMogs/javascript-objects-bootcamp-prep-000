2cd3841ad06b43dc201a30a19a697d20ad5dd826
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-09 06:31:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,5 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete ['artistName']playlist
+  delete playlist.artistName;
+  playlist;
 }
