3d4520f497d2e9bdb7c5a11069546ccdf558ee15
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-09 06:38:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
